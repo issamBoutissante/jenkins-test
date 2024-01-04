faf9666bee9702bd0be32ce32d501cd10e9984c2
Boutissante <73436159+issamBoutissante@users.noreply.github.com>
2024-01-04 14:56:02 +0100

    modified

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.example.Point.of.sale.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,18 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
     @GetMapping
-    public String hello(){
-        return "Hello from spring boot application :)";
+    public String hello() {
+        return "Hello Issam from spring boot application :)";
     }
 
     @GetMapping("/user")
-    public String getUsers(){
+    public String getUsers() {
         return "Users";
     }
 
-
     @GetMapping("/presentation")
-    public String getPresentation(){
+    public String getPresentation() {
         return "presentation";
     }
 }

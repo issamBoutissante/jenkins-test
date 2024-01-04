116547316afed98995e8856f9a449977853f8c69
Boutissante <73436159+issamBoutissante@users.noreply.github.com>
2024-01-04 15:16:09 +0100

    updated

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "Hello Issam from spring boot application :)";
+        return "Hello hi Issam from spring boot application :)";
     }
 
     @GetMapping("/user")
